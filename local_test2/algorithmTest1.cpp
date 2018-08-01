be9ea768d06c912c01f5c5ff49037ea4d1865c22
Unknown <sj0392@naver.com>
2018-08-01 17:29:23 +0900

    add algoritmeTest.cpp

---
+++ local_test2/algorithmTest1.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+#include <vector>
+
+void main()
+{
+    void printpicked(std::vector<int>& picked)
+    {
+        for(int n : picked){
+            cout<<n<<"";
+        }
+        cout<<endl;
+    }
+
+    void pick(int n, std::vector<int>& picked, int topick){
+        if(topick ==0){
+            printpicked(picked);
+            return;
+        }
+        int smallest = picked.empty()? 0:picked.back() +1;
+        for(int next = smallest; next <n; ++next){
+            picked.push_back(next);
+            pick(n,picked,topick-1);
+            picked.pop_back();
+        }
+    }
+}
